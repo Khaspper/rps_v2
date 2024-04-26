1123b76309318440398d6420e32d0bac006447fc
Markus <brandonnarciso24@gmail.com>
2024-04-25 18:02:26 -0700

    Add logic to winning and losing a single round
    so users can play one successful round

--- script.js
+++ script.js
@@ -9,14 +9,50 @@ function getComputerChoice() {
 }
 
 function getUserChoice() {
-    let weapon = prompt("Please choose a weapon from one of the following:\nRock\nPaper\nScissors")
+    let weapon = prompt("Please choose a weapon from one of the following:\nRock\nPaper\nScissors");
     if (!weapons.includes(weapon.toLowerCase())) {
-        console.log(`"${weapon}" is not a valid weapon, next time choose one of the following:\nRock\nPaper\nScissors`)
-        weapon = weapons[Math.floor(Math.random() * 3)]
-        console.log(`We have chosen a weapon for you: ${weapon}`)
-        return weapon
+        console.log(`"${weapon}" is not a valid weapon, next time choose one of the following:\nRock\nPaper\nScissors`);
+        weapon = weapons[Math.floor(Math.random() * 3)];
+        console.log(`We have chosen a weapon for you: ${weapon}`);
+        return weapon;
     }
     else
-        return weapon
+        return weapon.toLowerCase();
 }
 
+function fight(cpu = getComputerChoice(), player = getUserChoice()) {
+    switch (cpu) {
+        case "rock":
+            if (player == "paper") {
+                console.log("YOU WIN!")
+                humanScore += 1;
+            }
+            else if (player == "scissors") {
+                console.log("YOU LOSE!")
+                computerScore += 1
+            }
+            break;
+        case "paper":
+            if (player == "scissors") {
+                console.log("YOU WIN!")
+                humanScore += 1;
+            }
+            else if (player == "rock") {
+                console.log("YOU LOSE!")
+                computerScore += 1
+            }
+        case "scissors":
+            if (player == "rock") {
+                console.log("YOU WIN!")
+                humanScore += 1;
+            }
+            else if (player == "scissors") {
+                console.log("YOU LOSE!")
+                computerScore += 1
+            }
+            break;
+        default:
+            console.log("TIE!!")
+            break;
+    }
+}
